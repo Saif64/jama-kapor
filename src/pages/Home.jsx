79b5d359062985e3aa99fa1b73639310c4ea8578
Saif64 <saifmahmud81@gmail.com>
2022-06-14 00:04:32 +0600

    added slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
+
 import React from 'react'
+import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
-    <div className='container'>
-        <Navbar/>
+    <div>
+      <Announcement/>
+      <Navbar/>
+      <Slider/>
     </div>
   )
 }
