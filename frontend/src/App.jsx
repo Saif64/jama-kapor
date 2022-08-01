2106659dd0760035c36f23eb7f3ee11c585c2ecd
Saif64 <saifmahmud81@gmail.com>
2022-08-01 18:03:48 +0600

    connecting backend with router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,37 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  const user = true;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
