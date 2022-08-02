a0ad88c8e26a431a2e56adfa918c1242cb1d7f1a
Saif64 <saifmahmud81@gmail.com>
2022-08-02 20:06:55 +0600

    full backend completed after 13 hours

--- backend/app.js
+++ backend/app.js
@@ -21,12 +21,12 @@ app.use(fileUpload());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
-const payment = require("./routes/paymentRoute");
+// const payment = require("./routes/paymentRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
-app.use("/api/v1", payment);
+// app.use("/api/v1", payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
-const cloudinary = require("cloudinary");
+// const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
+const dotenv = require("dotenv");
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -10,18 +11,20 @@ process.on("uncaughtException", (err) => {
 });
 
 // Config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "backend/config/config.env" });
-}
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//   require("dotenv").config({ path: "backend/config/config.env" });
+// }
+
+dotenv.config({ path: "backend/config/config.env" });
 
 // Connecting to database
 connectDatabase();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
